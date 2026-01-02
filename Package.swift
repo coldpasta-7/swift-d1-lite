b449c35e6bb40d02ef023a24eb5835518dc33caf
Lemo-nade-room <lemo.nade.engineer.room@gmail.com>
2026-01-02 14:03:54 +0900

    style: 🎨 swift format

--- Package.swift
+++ Package.swift
@@ -4,23 +4,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-d1-sql-kit",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "swift-d1-sql-kit",
-            targets: ["swift-d1-sql-kit"]
-        ),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "swift-d1-sql-kit"
-        ),
-        .testTarget(
-            name: "swift-d1-sql-kitTests",
-            dependencies: ["swift-d1-sql-kit"]
-        ),
-    ]
+  name: "swift-d1-sql-kit",
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "swift-d1-sql-kit",
+      targets: ["swift-d1-sql-kit"]
+    )
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "swift-d1-sql-kit"
+    ),
+    .testTarget(
+      name: "swift-d1-sql-kitTests",
+      dependencies: ["swift-d1-sql-kit"]
+    ),
+  ]
 )
